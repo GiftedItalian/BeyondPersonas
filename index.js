f8f2bebb520f856b82b9a693382328bc0109144d
Carmine Straccia <58819190+GiftedItalian@users.noreply.github.com>
2022-03-17 18:50:57 +0000

    Initial files uploaded

---
+++ index.js
@@ -0,0 +1,15 @@
+
+$(document).scroll(function() {
+    if($(window).scrollTop() >= 0 &&  $(window).scrollTop() <= 100) {
+        document.getElementById("navbar").style.backgroundColor = "rgba(1, 1, 27," +  0.05 * $(window).scrollTop() + ")";
+    }
+    else{
+        document.getElementById("navbar").style.backgroundColor = "rgba(1, 1, 27, 1)";
+    }
+  });
+
+$(document).ready(function(){
+	$(".image").hover(function(){
+		$(this).toggleClass("gradient");
+	});
+});
